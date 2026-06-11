21188327799a81496537449e27f350bbb6920b19
Sebastian Boczek <39372825+Boczinator@users.noreply.github.com>
2026-06-11 12:43:43 +0200

    feat: add simple frontend form

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,12 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.use(cookieParser())
+	app.enableCors({
+		origin: 'http://localhost:5173',
+		credentials: true,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		allowedHeaders: 'Content-Type, Accept, Authorization',
+	})
 
 	await app.listen(process.env.PORT ?? 3000)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,122 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [error, setError] = useState(null)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+	const handleSubmit = async (event: SubmitEvent) => {
+		event.preventDefault()
 
-      <div className="ticks"></div>
+		try {
+			const response = await fetch('http://localhost:3000/auth/login', {
+				method: 'POST',
+				body: JSON.stringify({
+					email: email,
+					password: password,
+				}),
+				credentials: 'include',
+			})
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+			console.log(response)
+		} catch (error: any) {
+			setError(error)
+		}
+	}
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<div style={{ marginBottom: '20px' }}>
+					<label htmlFor="email">Email</label>
+					<input
+						type="text"
+						id="email"
+						onChange={(e) => setEmail(e.target.value)}
+					></input>
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input
+						type="text"
+						id="password"
+						onChange={(e) => setPassword(e.target.value)}
+					></input>
+				</div>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+				{error && <p>{error}</p>}
+
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	)
 }
 
 export default App
