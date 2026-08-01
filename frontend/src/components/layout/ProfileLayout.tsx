5829740edc15101277c3014cad6beb5adc1ba6e6
Sebastian Boczek <39372825+Boczinator@users.noreply.github.com>
2026-08-01 10:09:13 +0200

    fix. desktop scroll layout for auth view

--- frontend/src/components/layout/ProfileLayout.tsx
+++ frontend/src/components/layout/ProfileLayout.tsx
@@ -6,7 +6,7 @@ export const ProfileLayout = () => {
 		<div className="flex">
 			<AuthenticatedHeader />
 
-			<div className="flex-1 px-10 py-10">
+			<div className="flex-1 px-10 py-10 h-dvh overflow-y-auto">
 				<Outlet />
 			</div>
 		</div>
