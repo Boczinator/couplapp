62a9bff640413b5714ca1c44e694b8caa3b715ec
Sebastian Boczek <39372825+Boczinator@users.noreply.github.com>
2026-08-06 11:46:50 +0200

    feat: add websocket client test ping

--- frontend/src/socket/client.ts
+++ frontend/src/socket/client.ts
@@ -1,5 +1,9 @@
 import { io } from 'socket.io-client'
 
 const backendUrl = import.meta.env.VITE_PUBLIC_BACKEND_URL
+console.log(backendUrl)
+export const socket = io(backendUrl)
 
-const socket = io(backendUrl)
+socket.on('connect', () => {
+	console.log(1234)
+})
